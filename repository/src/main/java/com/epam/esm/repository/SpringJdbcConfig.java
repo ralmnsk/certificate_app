7fd1f74b3affe81a6e0dc0c48249ee4101ff29a3
ralmnsk <ralmnsk@gmail.com>
2020-06-29 09:16:23 +0300

    init module3

--- repository/src/main/java/com/epam/esm/repository/SpringJdbcConfig.java
+++ repository/src/main/java/com/epam/esm/repository/SpringJdbcConfig.java
@@ -1,70 +1,10 @@
 package com.epam.esm.repository;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
-
-/**
- * The type Spring jdbc config.
- * The configuration includes {@link DataSourceTransactionManager}
- * and {@link com.zaxxer.hikari.HikariDataSource} for creating of an configurable
- * connection pool.
- * The properties of the data source (connection pool)
- * are loaded from the repository.properties file.
- */
 @Configuration
-@PropertySource("classpath:repository.properties")
 @EnableTransactionManagement
 public class SpringJdbcConfig {
-
-    @Value("${jdbc}")
-    private String url;
-    @Value("${user}")
-    private String user;
-    @Value("${driver}")
-    private String driver;
-    @Value("${password}")
-    private String password;
-    @Value("${pool.size}")
-    private String poolSize;
-    @Value("${minimum.idle}")
-    private String minIdle;
-
-
-    /**
-     * The HikariCP pooled DataSource bean.
-     *
-     * @return the data source
-     */
-    @Bean
-    DataSource dataSource() {
-        final HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setDriverClassName(driver);
-        dataSource.setJdbcUrl(url);
-        dataSource.setUsername(user);
-        dataSource.setPassword(password);
-        dataSource.setMaximumPoolSize(Integer.parseInt(poolSize));
-        dataSource.setMinimumIdle(Integer.parseInt(minIdle));
-
-        return dataSource;
-    }
-
-    /**
-     * The transaction manager to control JDBC transactions.
-     *
-     * @return platform transaction manager
-     */
-
-    @Bean
-    public PlatformTransactionManager txManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
 
 }
--- web/src/main/java/com/epam/esm/web/Main.java
+++ web/src/main/java/com/epam/esm/web/Main.java
@@ -1,44 +1,13 @@
 package com.epam.esm.web;
 
-import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.startup.Tomcat;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-
-/**
- * The type Main.
- * The main application class that starts a rest service with embedded
- * Tomcat server.
- */
+@Configuration
+@SpringBootApplication
 public class Main {
-    private static final String PORT = "8082";
-    private static Logger logger = LoggerFactory.getLogger(Main.class);
-
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     * @throws Exception the exception
-     */
-    public static void main(String[] args) throws Exception {
-
-        String webappDirLocation = "./";
-        Tomcat tomcat = new Tomcat();
-
-        String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = PORT;
-        }
-
-        tomcat.setPort(Integer.parseInt(webPort));
-
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
-        logger.info("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
-
-        tomcat.start();
-        tomcat.getServer().await();
-
+    public static void main(String[] hello) {
+        SpringApplication.run(Main.class, hello);
     }
 }
