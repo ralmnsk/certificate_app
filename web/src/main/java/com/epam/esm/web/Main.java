f6029a1ed262a3301105c7ee75eb73d58f28267e
ralmnsk <ralmnsk@gmail.com>
2020-08-07 16:32:45 +0300

    SpringBootInitializer instead of WebInitializer

--- web/src/main/java/com/epam/esm/web/Main.java
+++ web/src/main/java/com/epam/esm/web/Main.java
@@ -3,6 +3,8 @@ package com.epam.esm.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -10,8 +12,13 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 @Configuration
 @SpringBootApplication
 @EnableSpringDataWebSupport
-public class Main {
+public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
     }
 }
--- web/src/main/java/com/epam/esm/web/config/SpringAppConfig.java
+++ web/src/main/java/com/epam/esm/web/config/SpringAppConfig.java
@@ -1,34 +1,34 @@
-package com.epam.esm.web.config;
-
-import com.epam.esm.exception.NoHandlerException;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletRegistration;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-public class SpringAppConfig implements WebApplicationInitializer {
-    @Override
-    public void onStartup(ServletContext container) {
-
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(AppConfig.class);
-        container.addListener(new ContextLoaderListener(rootContext));
-
-        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
-
-        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext) {
-            @Override
-            protected void noHandlerFound(HttpServletRequest request, HttpServletResponse response) throws Exception {
-                throw new NoHandlerException("No handler found");
-            }
-        });
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
-    }
-}
+//package com.epam.esm.web.config;
+//
+//import com.epam.esm.exception.NoHandlerException;
+//import org.springframework.web.WebApplicationInitializer;
+//import org.springframework.web.context.ContextLoaderListener;
+//import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+//import org.springframework.web.servlet.DispatcherServlet;
+//
+//import javax.servlet.ServletContext;
+//import javax.servlet.ServletRegistration;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//
+//public class SpringAppConfig implements WebApplicationInitializer {
+//    @Override
+//    public void onStartup(ServletContext container) {
+//
+//        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+//        rootContext.register(AppConfig.class);
+//        container.addListener(new ContextLoaderListener(rootContext));
+//
+//        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+//
+//        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext) {
+//            @Override
+//            protected void noHandlerFound(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//                throw new NoHandlerException("No handler found");
+//            }
+//        });
+//        dispatcher.setLoadOnStartup(1);
+//        dispatcher.addMapping("/");
+//    }
+//}
